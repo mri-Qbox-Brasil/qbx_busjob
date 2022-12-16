89dd30e0556edd68e05fc336d3de5036a50885dd
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-12-17 00:26:15 +0100

    feat(server): config for bonus chance

--- server/main.lua
+++ server/main.lua
@@ -1,6 +1,6 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-local function NearBus(src)
+local function isPlayerNearBus(src)
     local ped = GetPlayerPed(src)
     local coords = GetEntityCoords(ped)
     for _, v in pairs(Config.NPCLocations.Locations) do
@@ -16,12 +16,10 @@ RegisterNetEvent('qb-busjob:server:NpcPay', function()
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     if not Player then return end
-    if Player.PlayerData.job.name ~= "bus" or not NearBus(src) then return DropPlayer(src, 'Attempting to exploit') end
+    if Player.PlayerData.job.name ~= "bus" or not isPlayerNearBus(src) then return DropPlayer(src, 'Attempting to exploit') end
 
     local payment = math.random(15, 25)
-    local randomAmount = math.random(1, 5)
-    local r1, r2 = math.random(1, 5), math.random(1, 5)
-    if randomAmount == r1 or randomAmount == r2 then
+    if math.random(1, 100) < Config.BonusChance then
         payment += math.random(10, 20)
     end
     Player.Functions.AddMoney('cash', payment)
